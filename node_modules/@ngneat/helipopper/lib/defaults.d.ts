6a6c924ef5af4fdefcc2cdc175dcb2558cea07ab
drz10 <davidrodriguezrivera0@gmail.com>
2024-01-16 20:05:14 -0500

    Elimina la carpeta node_modules

--- node_modules/@ngneat/helipopper/lib/defaults.d.ts
+++
@@ -1,6 +0,0 @@
-import { TippyConfig } from './tippy.types';
-declare type Variation = TippyConfig['variations'][0];
-export declare const tooltipVariation: Variation;
-export declare const popperVariation: Variation;
-export declare function withContextMenuVariation(baseVariation: Variation): Variation;
-export {};
--- src/app/_services/UserLogin/loginTiidux.service.ts
+++ src/app/_services/UserLogin/loginTiidux.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment'; 
+
 import {  ApiResponse } from '../../dto/loginTiindux/genericResponse'; 
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 
   export class loginTiinduxService {
     private baseApiUrl = environment.apiUrlTiidux + 'api';
