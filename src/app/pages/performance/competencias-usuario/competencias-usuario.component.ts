65bda07e6904678c534773df570d4a11e865e7fa
drz10 <davidrodriguezrivera0@gmail.com>
2024-01-25 21:58:20 -0500

    carga inicial de preguntas y oprciones respuesta

--- src/app/pages/performance/competencias-usuario/competencias-usuario.component.ts
+++ src/app/pages/performance/competencias-usuario/competencias-usuario.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { ApiResponse } from '../../dto/loginTiindux/genericResponse';
+import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { reqPreguntas, reqrolespermisos } from 'src/app/Interfaces/UserLogin';
+import { loginTiinduxService } from 'src/app/_services/UserLogin/loginTiidux.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-competencias-usuario',
@@ -7,9 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompetenciasUsuarioComponent implements OnInit {
 
-  constructor() { }
+  preguntas: any[] = [];
+  pregunta: any = {};
+  respuestas: any[] = [];
+  respuesta: any = {};
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal, private router: Router, private loginServices: loginTiinduxService) { }
 
   ngOnInit(): void {
+
+
   }
+
+  cargalistas(){
+    this.loginServices.GetAllData<any>('Preguntas').subscribe((respuesta: ApiResponse<any>) => {
+      this.preguntas = respuesta.data;
+      console.log(respuesta.data)
+    });
+
+    this.loginServices.GetAllData<any>('opciones_respuesta').subscribe((respuesta: ApiResponse<any>) => {
+      this.respuestas = respuesta.data;
+      console.log(respuesta.data)
+    });
+
+ 
+  }
+
 
 }
